0895eae72fca1d13075d68f878cc3f17053ee51b
vd-dragan21 <43203539+vd-dragan21@users.noreply.github.com>
2021-11-10 18:41:21 +0100

    Create ub_0_e.cpp

---
+++ UB00/ub_0_e.cpp
@@ -0,0 +1,6 @@
+#include "fcpp.hh"
+
+int a = 1 ;
+int b = 1 ;
+std : : cout << cond ( a==b , "mach d i e s " , "mach das " ) << s td : : endl ;
+std : : cout << cond ( a==b , 1 , a>b) << s td : : endl ;
