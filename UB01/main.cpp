80b85377b8a1fd823926c4f9426d5cdad6e8fd4a
vd-dragan21 <43203539+vd-dragan21@users.noreply.github.com>
2021-11-10 22:40:54 +0100

    Add files via upload

--- UB01/main.cpp
+++ UB01/main.cpp
@@ -1,22 +1,41 @@
-
-// funktionale Programmierung: Header #include "fcpp.hh"
 #include "fcpp.hh"
 
-bool ungerade(int zahl){
-    int rest = zahl % 2;
-    if (rest == 0)
-        // kein Rest -> durch 2 teilbar
-        return false;
-    else
-        return true;
+// a)
+bool ungerade(int x) {
+    // ... implement me!
+    //return ???;
 }
 
-// als erste aufgerufene Funktion: main
-int main()
-{
-    return ungerade(2);
+// d)
+int multisignal (int y, int z) {
+    // ... implement me!
+    //return ???;
+}
+
+// b) & c)
+int quadrat (int x) {
+    // ... implement me!
+    //return ???;
+}
+
+// b) & d)
+int exponentation(int b, int ex) {
+    // ... implement me!
+    //return ???;
 }
 
 
+int main(int argc, char* argv[]) {
+    // mithilfe der Funktion enter_int aus fcpp.hh
+    // können wir Werte für Basis und Exponent
+    // über die Konsole einlesen und das
+    // ganze sogar funktional verpacken!
+    // Wichtig ist dann aber, dass du dieses Programm über eine Konsole ausführst!
+    return print(
+                exponentation(enter_int("Basis: "), enter_int("Exponent: "))
+                );
 
-
+    // Alternativ können wir auch die beiden Argumente Basis und Exponent
+    // dem programm als Übergabeparameter (argv) übergeben und mit readarg_int einlesen.
+    // return print(exponentation(readarg_int(argc,argv,1), readarg_int(argc,argv,2)));
+}
