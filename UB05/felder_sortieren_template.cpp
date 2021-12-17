40053e1efe6fe1a5c1c3192beb28d7cd1e1dd213
vd-dragan21 <43203539+vd-dragan21@users.noreply.github.com>
2021-12-17 13:10:49 +0100

    Update felder_sortieren_template.cpp

--- UB05/felder_sortieren_template.cpp
+++ UB05/felder_sortieren_template.cpp
@@ -1,3 +1,6 @@
+// Group: Valeriia Dragan, Ben Ishaia, Selina Ernst
+#include <iostream>
+using namespace std;
 #include "fcpp.hh"
 
 const int ggroesse = 10;  // globale Variable ggroesse
@@ -11,16 +14,41 @@ void feld_ausgeben   (int anzahl)
     std::cout << std::endl;
 }
 
-void feld_bubblesort ( int anzahl );
+void feld_bubblesort ( int anzahl )
+{
+    int temp;
+    for(int i = 0; i < anzahl - 1; i++){    //gehe durch das Feld
+        if(gfeld[i]>gfeld[i+1]){    //vergleiche die benachbarten Werte miteinander an Positionen i & i+1
+            temp = gfeld[i];    //tausche die Werte an Positionen i & i+1, wobei Wert aus Position i in einer temporären Variable gespeichert wird
+            gfeld[i] = gfeld[i+1];
+            gfeld[i+1] = temp;
+            }
+        }
+    }
 
-void feld_insertionsort ( int anzahl );
+void feld_insertionsort ( int anzahl )
+{
+    int k, i, j;
+    for (k = 1; k < anzahl; k++)    //gehe durch das Feld
+    {
+        i = gfeld[k];
+        j = k - 1;
+        while (j >= 0 && gfeld[j] > i)  //füge den neuen elemnt in die sortierte Folge ein
+        {
+            gfeld[j + 1] = gfeld[j];
+            j = j - 1;
+        }
+        gfeld[j + 1] = i;   //wähle das nächste neue Element, das noch nicht sortiert wurde
+    }
+}
 
 int main( int argc, char *argv[] ){
-    int anzahl = argc-1;             // anzahl ist Anzahl der eingegebenen Listenelemente
+    int anzahl = argc-1;
+    // anzahl ist Anzahl der eingegebenen Listenelemente
     if ( anzahl <= ggroesse ){
          for ( int i = 1; i <= anzahl; i = i+1 ) // trage die Liste in gfeld ein
             gfeld[i-1] = readarg_int( argc, argv, i );
-         //feld_bubblesort( anzahl );
+         feld_bubblesort( anzahl );
          feld_insertionsort( anzahl );
          feld_ausgeben( anzahl );
     }
